8fac540c80e4423328de497ddc2c371ecf45342a
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-07-26 22:23:52 +0900

    TW-58

---
+++ RESTfulAPI/routes/README.md
@@ -0,0 +1,5 @@
+## routes/api
+routes/api는 controllers/api에서 생성한 메소드를 import 해서 사용
+
+## routes/rest
+routes/rest는 controllers/rest에서 생성한 메소드를 import 해서 사용
--- RESTfulAPI/routes/api/users.route.js
+++ RESTfulAPI/routes/api/users.route.js
@@ -5,3 +5,7 @@ import {
 } from '../../controllers/api/user.controller'
 
 const router = express.Router()
+
+router.route('/login').post( ( req , res , next ) => {
+    
+})
